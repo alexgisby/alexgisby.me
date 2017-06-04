13345209fe147620f39d54d6a75d3c1db72b965b
Alex Gisby <alex@solution10.com>
2017-06-04 10:19:13 +0100

    Universal / Isometric goodness.

--- app/js/server.js
+++ app/js/server.js
@@ -1,15 +1,40 @@
-const express = require('express');
-const path = require('path');
+import path from 'path';
+import Express from 'express';
+import React from 'react';
+import {renderToString} from 'react-dom/server';
+import {match, RouterContext} from 'react-router';
+import routes from './routes';
 
-const app = new express();
+const app = new Express();
 
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'hbs');
 
-app.use(express.static('build'));
+app.use(Express.static('build'));
 
-app.get('/', (req, res) => {
-    res.render('layout')
+app.get('*', (req, res) => {
+    match(
+        {routes, location: req.url},
+        (err, redirectLocation, renderProps) => {
+            if (err) {
+                return res.status(500).send(err.message);
+            }
+
+            if (redirectLocation) {
+                return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+            }
+
+            let markup;
+            if (renderProps) {
+                markup = renderToString(<RouterContext {...renderProps}/>)
+            } else {
+                markup = renderToString(<NotFoundPage/>);
+                res.status(404);
+            }
+
+            return res.render('layout', {markup});
+        }
+    )
 });
 
 module.exports = app;
